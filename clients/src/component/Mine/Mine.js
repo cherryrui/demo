eaaf19ddc9509e1eba5f3e0f79f643e44c32437c
cherryrui <397420713@qq.com>
2017-09-04 14:13:38 +0800

    wf：首页、中英转换

--- clients/src/component/Mine/Mine.js
+++ clients/src/component/Mine/Mine.js
@@ -23,6 +23,7 @@ class Mine extends React.Component {
         super(props);
         this.state = {
             user: JSON.parse(localStorage.user),
+            select: 0,
         }
     }
 
@@ -36,16 +37,13 @@ class Mine extends React.Component {
             <div className={css.menu}>
             {operator.menu.map(menu=> {
                 return <div>
-                    <p>
-                        <Link to="main/">
-                            <FormattedMessage id="app.category" defaultMessage="分类"/>
-                        </Link>
+                    <p className={css.menu_title}>
+                        <FormattedMessage id="app.category" defaultMessage="分类"/>
                     </p>
                     {menu.list.map(item=> {
-                        return <p>
-                            <Link to="main/">
-                                <FormattedMessage id="app.category" defaultMessage="分类"/>
-                            </Link>
+                        return <p className={this.state.select==item.key?css.active:css.item}>
+                            <FormattedMessage id="app.category" defaultMessage="分类"/>
+
                         </p>
                     })}
                 </div>
--- clients/src/component/Mine/operator.js
+++ clients/src/component/Mine/operator.js
@@ -6,23 +6,23 @@ export default {
         {
             name: "mine.me",
             list: [
-                {code: 1000, title: "mine.xx", url: "/main/"},
-                {code: 1001, title: "mine.xx", url: "/main/"},
-                {code: 1002, title: "mine.xx", url: "/main/"},
+                {key: 1000, title: "mine.xx", url: "/main/"},
+                {key: 1001, title: "mine.xx", url: "/main/"},
+                {key: 1002, title: "mine.xx", url: "/main/"},
             ]
         }, {
             name: "mine.me",
             list: [
-                {code: 1000, title: "mine.xx", url: "/main/"},
-                {code: 1001, title: "mine.xx", url: "/main/"},
-                {code: 1002, title: "mine.xx", url: "/main/"},
+                {key: 2000, title: "mine.xx", url: "/main/"},
+                {key: 2001, title: "mine.xx", url: "/main/"},
+                {key: 2002, title: "mine.xx", url: "/main/"},
             ]
         }, {
             name: "mine.me",
             list: [
-                {code: 1000, title: "mine.xx", url: "/main/"},
-                {code: 1001, title: "mine.xx", url: "/main/"},
-                {code: 1002, title: "mine.xx", url: "/main/"},
+                {key: 3000, title: "mine.xx", url: "/main/"},
+                {key: 3001, title: "mine.xx", url: "/main/"},
+                {key: 3002, title: "mine.xx", url: "/main/"},
             ]
         },
     ]
