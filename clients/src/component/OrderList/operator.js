b07943b308c4e77a1ceec3447a55d4225dbedcc5
cherryrui <397420713@qq.com>
2017-09-19 14:05:05 +0800

    wf：修改供应组件名称

---
+++ clients/src/component/OrderList/operator.js
@@ -0,0 +1,23 @@
+export default {
+	order_status: [{
+		icon: "",
+		value: 1,
+		key: "orderlist.all.order"
+	}, {
+		icon: "",
+		value: 2,
+		key: "orderlist.unpay"
+	}, , {
+		icon: "",
+		value: 3,
+		key: "orderlist.instal.unpay"
+	}, {
+		icon: "",
+		value: 4,
+		key: "orderlist.instal.unpay"
+	}, {
+		icon: "",
+		value: 5,
+		key: "orderlist.need.pay"
+	}, ]
+}
