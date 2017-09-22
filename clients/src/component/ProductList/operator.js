a6045d434574cb85affde9383a173939549d226f
cherryrui <397420713@qq.com>
2017-09-22 22:50:13 +0800

    wf：修改供应组件名称

---
+++ clients/src/component/ProductList/operator.js
@@ -0,0 +1,18 @@
+export default {
+	sort: [{
+		key: 1,
+		id: "sort.new",
+		default: "新品",
+		orderType: "desc"
+	}, {
+		key: 2,
+		id: "sort.price",
+		default: "价格",
+		orderType: "asc"
+	}, {
+		key: 3,
+		id: "sort.sale",
+		default: "销量",
+		orderType: "desc"
+	}, ]
+}
