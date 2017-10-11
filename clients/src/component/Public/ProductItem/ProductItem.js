b4cbf49db094c20aca53f94f9c6fa2ad8b223dd0
cherryrui <397420713@qq.com>
2017-10-11 18:16:52 +0800

    wf：修改供应组件名称

---
+++ clients/src/component/Public/ProductItem/ProductItem.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import css from './ProductItem.scss';
+import {
+    Link
+} from 'react-router';
+import {
+    FormattedMessage,
+} from 'react-intl';
+
+class ProductItem extends React.Component {
+
+
+    render() {
+        return <div className={this.props.className}>
+			<Link to={"page/product-detail/"+this.props.product.productId} className={css.table_product}>
+                <img src={this.props.product.coverUrl}/>
+                <div className={css.info}>
+                    <p className={css.name}>{this.props.product.productName}</p>
+                    <p>
+                        <FormattedMessage id="app.brand" defaultMessage="我的购物车"/>：
+                        {locale=="en"?this.props.product.brandNameEn:this.props.product.brandNameCn}
+                    </p>
+                    {this.props.hiddenMoq?"":<p>
+                        <FormattedMessage id="product.detail.MOQ" defaultMessage="我的购物车"/>
+                        ：{this.props.product.moq}
+                    </p>}
+                    <p>
+                        <FormattedMessage id="mine.product.No" defaultMessage="我的购物车"/>
+                        ：{this.props.product.productNo}</p>
+                </div>
+            </Link>
+		</div>
+    }
+}
+
+export default ProductItem;
